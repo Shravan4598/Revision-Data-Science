cf6630093d325dde7b74ec363f0c0ca39e74a0ea
Shravan Kumar Pandey <166730155+Shravan4598@users.noreply.github.com>
2026-07-31 01:44:53 +0530

    ALTER DELETE COLUMNS

---
+++ SQL/SQL Revision/015.DDL_ALTER_DELETE_COLUMN.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE IF NOT EXISTS csvtu;
+USE csvtu;
+
+
+CREATE TABLE IF NOT EXISTS student (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    age INT,
+    CONSTRAINT students_age_check CHECK (age > 6 AND age < 25)
+);
+
+INSERT INTO student (name, age)
+VALUES
+('Rahul Sharma', 18),
+('Priya Singh', 20),
+('Amit Kumar', 19),
+('Neha Verma', 21),
+('Rohit Gupta', 17),
+('Anjali Patel', 22),
+('Vikas Yadav', 23),
+('Sneha Mishra', 16),
+('Karan Mehta', 24),
+('Pooja Jain', 15);
+
+SELECT * FROM student;
+
+ALTER TABLE student
+ADD COLUMN password VARCHAR(255);
+
+UPDATE student
+SET password = CASE student_id
+    WHEN 1 THEN 'rahul123'
+    WHEN 2 THEN 'priya456'
+    WHEN 3 THEN 'amit789'
+    WHEN 4 THEN 'neha321'
+    WHEN 5 THEN 'rohit654'
+    WHEN 6 THEN 'anjali987'
+    WHEN 7 THEN 'vikas111'
+    WHEN 8 THEN 'sneha222'
+    WHEN 9 THEN 'karan333'
+    WHEN 10 THEN 'pooja444'
+END;
+
+ALTER TABLE student ADD COLUMN surname VARCHAR(35) NOT NULL DEFAULT "Kumar" AFTER name;
+
+ALTER TABLE student
+ADD COLUMN cgpa INT,
+ADD COLUMN adm_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE  student DROP COLUMN cgpa;
+
+SELECT * FROM student;
+
+
