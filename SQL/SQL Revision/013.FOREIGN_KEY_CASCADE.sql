4b332ad77357ad1e2eb4f863964d413b0167d8a8
Shravan Kumar Pandey <166730155+Shravan4598@users.noreply.github.com>
2026-07-31 01:03:11 +0530

    CASCADE

---
+++ SQL/SQL Revision/013.FOREIGN_KEY_CASCADE.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS csvtu;
+USE csvtu;
+
+CREATE TABLE IF NOT EXISTS orders(
+order_id VARCHAR(80) PRIMARY KEY,
+customer_id INT,
+order_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+CONSTRAINT orders_fk FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+INSERT INTO orders (order_id, customer_id)
+VALUES
+('ORD001', 1),
+('ORD002', 2),
+('ORD003', 1),
+('ORD004', 3);
+
+SELECT * FROM orders;
+
+UPDATE customers
+SET customer_id = 20
+WHERE customer_id = 2;
+
+
