a47f1fb2eb050059555f2c9a19a935223fac8fbb
Shravan Kumar Pandey <166730155+Shravan4598@users.noreply.github.com>
2026-07-30 22:14:33 +0530

    CHECK

---
+++ SQL/SQL Revision/008.DDL_constraints.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS csvtu;
+USE csvtu;
+
+CREATE TABLE IF NOT EXISTS student(
+student_id INT PRIMARY KEY AUTO_INCREMENT,
+name  VARCHAR(50) NOT NULL,
+age INT,
+
+CONSTRAINT students_age_check CHECK (age>6 AND age<25)
+);
+INSERT INTO student (name, age)
+VALUES
+('Rahul Sharma', 18),
+('Priya Singh', 20),
+('Amit Kumar', 19),
+('Neha Verma', 21),
+('Rohit Gupta', 17),
+('Anjali Patel', 22),
+('Vikas Yadav', 23),
+('Sneha Mishra', 16),
+('Karan Mehta', 24),
+('Pooja Jain', 15);
+
+SELECT * FROM student;
