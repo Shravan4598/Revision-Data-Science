c9a2f5f4c7bb446e697c2ee1e0395a56153b06f9
Shravan Kumar Pandey <166730155+Shravan4598@users.noreply.github.com>
2026-07-30 21:44:52 +0530

    PRIMARY KEY

---
+++ SQL/SQL Revision/004.DDL_constraints.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS csvtu;
+USE csvtu;
+
+CREATE TABLE IF NOT EXISTS users(
+user_id INT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(100) NOT NULL,
+password VARCHAR(250) NOT NULL,
+
+CONSTRAINT users_email_unique UNIQUE (email),
+CONSTRAINT users_name_user_id_unique UNIQUE (user_id,name)
+);
+
+INSERT INTO users (user_id, name, email, password)
+VALUES
+(1, 'Rahul Sharma', 'rahul.sharma@gmail.com', 'rahul123'),
+(2, 'Priya Singh', 'priya.singh@gmail.com', 'priya456'),
+(3, 'Amit Kumar', 'amit.kumar@gmail.com', 'amit789'),
+(4, 'Neha Verma', 'neha.verma@gmail.com', 'neha321'),
+(5, 'Rohit Gupta', 'rohit.gupta@gmail.com', 'rohit654'),
+(6, 'Anjali Patel', 'anjali.patel@gmail.com', 'anjali987'),
+(7, 'Vikas Yadav', 'vikas.yadav@gmail.com', 'vikas111'),
+(8, 'Sneha Mishra', 'sneha.mishra@gmail.com', 'sneha222'),
+(9, 'Karan Mehta', 'karan.mehta@gmail.com', 'karan333'),
+(10, 'Pooja Jain', 'pooja.jain@gmail.com', 'pooja444');
+
+SELECT * FROM users;
+
+DROP TABLE users;
