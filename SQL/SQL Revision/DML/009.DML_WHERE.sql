59accd674f5b21fc02fa346a58760172444ab701
Shravan Kumar Pandey <166730155+Shravan4598@users.noreply.github.com>
2026-07-31 14:53:48 +0530

    WHERE
    It is used to filter.

---
+++ SQL/SQL Revision/DML/009.DML_WHERE.sql
@@ -0,0 +1,6 @@
+SELECT * FROM csvtu.smartphones
+WHERE brand_name='samsung';
+
+SELECT * FROM csvtu.smartphones
+WHERE brand_name='apple';
+
---
+++ SQL/SQL Revision/DML/010.DML_WHERE.sql
@@ -0,0 +1,9 @@
+SELECT * FROM csvtu.smartphones
+WHERE price>50000;
+
+
+SELECT * FROM csvtu.smartphones
+WHERE price>100000;
+
+SELECT * FROM csvtu.smartphones
+WHERE price<10000;
