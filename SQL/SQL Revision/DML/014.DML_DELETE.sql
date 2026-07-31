b745f76c9ec5a9c5b01c1b98f08cb3265c7bb363
Shravan Kumar Pandey <166730155+Shravan4598@users.noreply.github.com>
2026-07-31 20:13:36 +0530

    DELETE

---
+++ SQL/SQL Revision/DML/014.DML_DELETE.sql
@@ -0,0 +1,11 @@
+SELECT * FROM csvtu.smartphones WHERE price>200000;
+
+DELETE FROM csvtu.smartphones WHERE price>200000;
+
+SELECT * FROM csvtu.smartphones WHERE price>200000;
+
+SELECT * FROM csvtu.smartphones WHERE primary_camera_rear>150; 
+
+DELETE  FROM csvtu.smartphones WHERE primary_camera_rear>150 AND  brand_name='samsung';
+
+SELECT * FROM csvtu.smartphones WHERE primary_camera_rear>150 AND brand_name='samsung'; 
