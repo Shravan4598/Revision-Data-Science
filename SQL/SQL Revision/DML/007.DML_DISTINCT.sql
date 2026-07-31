87bcdf32c86188209577443e669ee6d346675377
Shravan Kumar Pandey <166730155+Shravan4598@users.noreply.github.com>
2026-07-31 14:38:47 +0530

    DISTINCT
    It is used to find the unique values in a column.

---
+++ SQL/SQL Revision/DML/007.DML_DISTINCT.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT(brand_name) AS 'all_brands' FROM csvtu.smartphones;
+
+SELECT DISTINCT(processor_brand) AS 'all_processors_brands' FROM csvtu.smartphones;
+
+SELECT DISTINCT(os) AS 'all_operating_systems' FROM csvtu.smartphones;
