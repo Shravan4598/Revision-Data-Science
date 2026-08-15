d1f33247264f5027630cf9d30cf7d7b5fec9c0ab
Shravan Kumar Pandey <166730155+Shravan4598@users.noreply.github.com>
2026-08-16 01:55:48 +0530

    Delete SQL/SQL Revision/String Functions/Q2.sql

--- SQL/SQL Revision/String Functions/Q2.sql
+++
@@ -1,9 +0,0 @@
-
-
--- Q2.Display the movies name and its director with star?
--- Movie_name ---> Director_name and Star_name
-
-
-USE movie;
-
-SELECT CONCAT(name," ---> ",director, ' and ',star) FROM movies;
